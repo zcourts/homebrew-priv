a63bdb4bb253ef6bb83a203b99ee710867bd4cbc
ichizok <gclient.gaap@gmail.com>
2015-01-22 21:06:37 +0900

    ttcp: fixed audit problem

--- ttcp.rb
+++ ttcp.rb
@@ -1,15 +1,17 @@
-require 'formula'
-
 class Ttcp < Formula
-  url 'http://www.pcausa.com/Utilities/pcattcp/UnixTTCP.zip'
-  homepage 'http://www.pcausa.com/Utilities/pcattcp.htm'
-  md5 '4061bf263cdd6bbccacedcca9b6370aa'
-  version '1.12'
+  homepage "http://www.pcausa.com/Utilities/pcattcp.htm"
+  url "http://www.pcausa.com/Utilities/pcattcp/UnixTTCP.zip"
+  sha1 "a8bf785174d20517298ad85f0f5a8166525cac93"
+  version "1.12"
 
   def install
     inreplace "makefile", "gcc", "#{ENV.cc} #{ENV.cflags}"
 
-    system "make ttcp"
+    system "make", "ttcp"
     bin.install "ttcp"
+  end
+
+  test do
+    #
   end
 end
